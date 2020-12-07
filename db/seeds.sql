b73343677789dd1ce96c48a531e5fec87aa07bef
Ryan Zelazny <RyanZelazny@gmail.com>
2020-12-06 19:41:05 -0500

    new seed values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Veggie burger', true);
